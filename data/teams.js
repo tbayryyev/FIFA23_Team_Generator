c83ecfb7f4dca58a3eff71ae922a37aef21fc520
Tahyr Bayryyev <mrtahyr@gmail.com>
2022-12-25 17:25:16 -0500

    team data file

--- data/teams.js
+++ data/teams.js
@@ -2,3 +2,6 @@ const mongoCollections = require("../config/mongoCollections");
 const teams = mongoCollections.teams;
 const { ObjectId } = require('mongodb');
 
+async function createTeam(team_name,team_image,star_rating,league){
+
+}
